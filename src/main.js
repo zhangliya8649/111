fc864297e9940d7617161871b111a164762dbe23
liubing <liubing@spap.com>
2019-01-31 10:49:28 +0800

    解决冲突

--- src/main.js
+++ src/main.js
@@ -1,16 +1,10 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import router from './router'
 import App from './App'
-<<<<<<< HEAD
-import ElementUI from'element-ui'
+import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-=======
-import Action from './request/Action.js'
-import Http from './request/http.js'
-
->>>>>>> 9d67773257f844f9e22cae14fe7631e88753530d
+import Action from './request/Action'
+import Http from './request/http'
 
 Vue.use(ElementUI)
 Vue.config.productionTip = false
@@ -18,11 +12,9 @@ Vue.config.productionTip = false
 Vue.prototype.Http = Http
 Vue.prototype.Action = Action
 
-
-/* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  components: {App},
-  template: '<App/>'
+    el: '#app',
+    router,
+    components:{App},
+    template:'<App/>'
 })
