3d548cb13dea165d9961fcaeccfd6f57e644c243
LiangShaoQi <liangshaoqi@shixinyun.com>
2019-03-08 11:05:57 +0800

    修复菜单切换找不到清除方法bug

---
+++ src/components/upload/upload.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="upload-component">
+        上传组件
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return{}
+    },
+    mounted() {},
+    methods: {
+
+    }
+}
+</script>
+<style lang='scss' scoped>
+
+</style>
+
+
